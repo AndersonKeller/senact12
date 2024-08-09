19d7636669d6d7dccec53dc448db94abe1c35a0f
AndersonKeller <andersonkellervendas@hotmail.com>
2024-08-08 21:26:48 -0300

    08-08 modal, templateString

--- formularios/script.js
+++ formularios/script.js
@@ -1,4 +1,5 @@
 function login(){
+    const mensagem = "usuário ou senha inválidos, por favor, verifique seus dados!"
     const form = document.querySelector("form")
     const button = document.querySelector("button")
     const email = document.querySelector("#email")
@@ -14,7 +15,28 @@ function login(){
         if(password.value != "1234" ){
             console.log("Senha errada")
             console.log(window)
-            
+            const body = document.querySelector("body")
+            body.insertAdjacentHTML("afterend",
+                `
+                 <div class="modal_wrapper">
+        <div class="modal">
+            <div class="modal_header">
+                <h2>Atenção</h2>
+                <button id="close">X</button>
+            </div>
+            <div class="modal_body">
+                <p>${mensagem}</p>
+                <button>Ok</button>
+            </div>
+            </div>
+        </div>
+                `
+            )
+            const btnClose = document.querySelector("#close")
+            btnClose.addEventListener("click",()=>{
+                const modal = document.querySelector(".modal_wrapper")
+                modal.remove()
+            })
         }else{
             console.log("senha correta")
             window.location.href = "./home"
